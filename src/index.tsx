80a7909aaab50732adfc2a706bf7623d0a3813e3
CK <cheekhuin.loong@gmail.com>
2020-07-19 21:30:21 +0800

    added service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './store'
+import * as serviceWorker from './serviceWorker';
 
 import './index.css'
 
@@ -13,3 +14,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
+
+serviceWorker.register();
